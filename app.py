4d4097cddc00bcb60513c398cdb125651b677f43
Royston <roymenezes28@gmail.com>
2025-08-19 14:37:07 +0530

    MongoDB For Hosting

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config.db import init_db
 from app.routes.auth import auth_bp
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -9,5 +10,6 @@ def create_app():
     return app
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=port)
