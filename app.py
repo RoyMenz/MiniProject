4f3d3b07b9472245190464708f1c43eb4258eaf8
Royston <roymenezes28@gmail.com>
2025-08-19 14:40:39 +0530

    create app function fixed

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-from flask import Flask
-from config.db import init_db
-from app.routes.auth import auth_bp
-import os
+# app.py (top-level file)
 
-def create_app():
-    app = Flask(__name__)
-    init_db(app)
-    app.register_blueprint(auth_bp)
-    return app
+from app import create_app  # import factory from app/__init__.py
+
+app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app = create_app()
-    app.run(debug=True,host="0.0.0.0",port=port)
+    app.run(host="0.0.0.0", port=5000)
